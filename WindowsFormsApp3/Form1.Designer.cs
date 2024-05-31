98eec701b269c57f30b7078be6f1b78ffb012f66
Eustace831697 <eustace831697@gmail.com>
2024-06-01 01:02:36 +0800

    初次上傳

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.url = new System.Windows.Forms.TextBox();
+            this.PathDir = new System.Windows.Forms.Button();
+            this.Save = new System.Windows.Forms.Button();
+            this.Loadfile = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SaveList = new System.Windows.Forms.ListBox();
+            this.Load_Save_Data_List = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // url
+            // 
+            this.url.Location = new System.Drawing.Point(38, 41);
+            this.url.Name = "url";
+            this.url.Size = new System.Drawing.Size(846, 29);
+            this.url.TabIndex = 0;
+            this.url.TextChanged += new System.EventHandler(this.url_TextChanged);
+            // 
+            // PathDir
+            // 
+            this.PathDir.Location = new System.Drawing.Point(38, 89);
+            this.PathDir.Name = "PathDir";
+            this.PathDir.Size = new System.Drawing.Size(140, 45);
+            this.PathDir.TabIndex = 1;
+            this.PathDir.Text = "選擇存檔路徑";
+            this.PathDir.UseVisualStyleBackColor = true;
+            this.PathDir.Click += new System.EventHandler(this.Path_Click);
+            // 
+            // Save
+            // 
+            this.Save.Location = new System.Drawing.Point(932, 210);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(110, 45);
+            this.Save.TabIndex = 2;
+            this.Save.Text = "存檔";
+            this.Save.UseVisualStyleBackColor = true;
+            this.Save.Click += new System.EventHandler(this.Save_Click);
+            // 
+            // Loadfile
+            // 
+            this.Loadfile.Location = new System.Drawing.Point(932, 283);
+            this.Loadfile.Name = "Loadfile";
+            this.Loadfile.Size = new System.Drawing.Size(110, 45);
+            this.Loadfile.TabIndex = 3;
+            this.Loadfile.Text = "讀取";
+            this.Loadfile.UseVisualStyleBackColor = true;
+            this.Loadfile.Click += new System.EventHandler(this.Load_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // SaveList
+            // 
+            this.SaveList.FormattingEnabled = true;
+            this.SaveList.ItemHeight = 18;
+            this.SaveList.Location = new System.Drawing.Point(38, 210);
+            this.SaveList.Name = "SaveList";
+            this.SaveList.Size = new System.Drawing.Size(846, 454);
+            this.SaveList.TabIndex = 4;
+            // 
+            // Load_Save_Data_List
+            // 
+            this.Load_Save_Data_List.Location = new System.Drawing.Point(910, 31);
+            this.Load_Save_Data_List.Name = "Load_Save_Data_List";
+            this.Load_Save_Data_List.Size = new System.Drawing.Size(132, 45);
+            this.Load_Save_Data_List.TabIndex = 5;
+            this.Load_Save_Data_List.Text = "載入存檔資料";
+            this.Load_Save_Data_List.UseVisualStyleBackColor = true;
+            this.Load_Save_Data_List.Click += new System.EventHandler(this.Load_Save_Data_List_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(144F, 144F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(1141, 767);
+            this.Controls.Add(this.Load_Save_Data_List);
+            this.Controls.Add(this.SaveList);
+            this.Controls.Add(this.Loadfile);
+            this.Controls.Add(this.Save);
+            this.Controls.Add(this.PathDir);
+            this.Controls.Add(this.url);
+            this.Name = "Form1";
+            this.Text = "存檔備份";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox url;
+        private System.Windows.Forms.Button PathDir;
+        private System.Windows.Forms.Button Save;
+        private System.Windows.Forms.Button Loadfile;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.ListBox SaveList;
+        private System.Windows.Forms.Button Load_Save_Data_List;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.WebRequestMethods;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Path_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog folderBrowserDialog1 = new FolderBrowserDialog())
+            {
+                // 設定對話框的屬性
+                folderBrowserDialog1.Description = "選擇資料夾";
+                folderBrowserDialog1.RootFolder = Environment.SpecialFolder.MyComputer; // 設定根資料夾
+
+                // 如果使用者選擇了資料夾並按下了「確定」按鈕
+                if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    // 取得選擇的資料夾路徑
+                    string folderPath = folderBrowserDialog1.SelectedPath;
+
+                    // 將路徑顯示在標籤中或做其他處理
+                    url.Text = folderPath;
+                }
+            } // 使用完 FolderBrowserDialog 後，會自動釋放資源
+        }
+
+        private void CopyFolder(string sourceFolder, string destFolder)
+        {
+            // 如果目的地資料夾不存在，則建立它
+            if (!Directory.Exists(destFolder))
+            {
+                Directory.CreateDirectory(destFolder);
+            }
+
+            // 複製資料夾內的所有檔案
+            foreach (string file in Directory.GetFiles(sourceFolder))
+            {
+                string destFile = Path.Combine(destFolder, Path.GetFileName(file));
+                System.IO.File.Copy(file, destFile, true);
+            }
+
+            // 複製資料夾內的所有子資料夾（遞迴）
+            foreach (string folder in Directory.GetDirectories(sourceFolder))
+            {
+                string destSubFolder = Path.Combine(destFolder, Path.GetFileName(folder));
+                CopyFolder(folder, destSubFolder);
+            }
+        }
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+            // 取得選擇的資料夾路徑
+            string sourceFolderPath = url.Text.Trim();
+
+            if (string.IsNullOrEmpty(sourceFolderPath))
+            {
+                MessageBox.Show("請選擇資料夾路徑");
+                url.Focus();
+                return;
+            }
+
+            // 取得資料夾的父資料夾路徑
+            string parentFolderPath = Directory.GetParent(sourceFolderPath).FullName;
+
+            string[] subDirectories = Directory.GetDirectories(parentFolderPath);
+
+            // 新資料夾的名稱為原資料夾名稱加上"_1"
+            string newFolderName = Path.GetFileName(sourceFolderPath) + "_" + subDirectories.Length.ToString();
+
+            // 新資料夾的完整路徑
+            string newFolderPath = Path.Combine(Path.GetDirectoryName(sourceFolderPath), newFolderName);
+
+            //如果有選中項 改用選中項的路徑
+            if (SaveList.SelectedIndex > -1)
+            {
+                newFolderPath = SaveList.SelectedItem.ToString();                
+            }
+
+            // 建立新資料夾
+            Directory.CreateDirectory(newFolderPath);
+
+            // 複製資料夾內的所有檔案及子資料夾至新資料夾中
+            CopyFolder(sourceFolderPath, newFolderPath);
+
+            MessageBox.Show("資料夾及其內容已成功複製至新資料夾中。");
+
+            bool itemExists = false;
+            foreach (var item in SaveList.Items)
+            {
+                // 如果要检查的项已经存在于 ListBox 中，将 itemExists 设置为 true，并跳出循环
+                if (item.ToString() == newFolderPath)
+                {
+                    itemExists = true;                   
+                }
+            }
+
+            if (!itemExists)
+            {
+                SaveList.Items.Add(newFolderPath);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Load_Click(object sender, EventArgs e)
+        {
+            if (SaveList.SelectedItems.Count < 1)
+            {
+                MessageBox.Show("請選擇欲讀取的備份檔");
+                return;
+            }
+
+            // 取得選擇的資料夾路徑
+            string sourceFolderPath = SaveList.SelectedItem.ToString();
+
+            //目標資料夾
+            string targetFolder = url.Text.Trim();
+
+            // 取得指定資料夾中的所有檔案
+            string[] files = Directory.GetFiles(sourceFolderPath);
+
+            CopyFolder(sourceFolderPath, targetFolder);
+
+            // 複製資料夾中的所有檔案到目標資料夾
+            //foreach (string file in files)
+            //{
+            //    string fileName = Path.GetFileName(file);
+            //    string destFile = Path.Combine(targetFolder, fileName);
+            //    System.IO.File.Copy(file, destFile, true);
+            //}
+
+            MessageBox.Show("檔案已成功複製至目標資料夾！", "完成", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            SaveList.SelectedIndex = -1;
+        }
+
+        private void url_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Load_Save_Data_List_Click(object sender, EventArgs e)
+        {
+            string urlString = url.Text.Trim(); //檔案路徑
+            if (string.IsNullOrEmpty(urlString))
+            {
+                MessageBox.Show("請選擇目標資料夾路徑");
+                url.Focus();
+                return;
+            }
+
+            SaveList.Items.Clear(); //移除所有項目
+
+            string parentFolderPath = Directory.GetParent(urlString).FullName;
+
+            string[] subDirectories = Directory.GetDirectories(parentFolderPath);
+
+            Array.Sort(subDirectories, StringComparer.CurrentCultureIgnoreCase);
+
+            foreach (string i in subDirectories)
+            {                
+                if (i != urlString)
+                {
+                    SaveList.Items.Add(i);
+                }
+            }
+
+            if (SaveList.Items.Count < 1)
+            {
+                MessageBox.Show("目前暫無備份，請按下存檔進行備份。");
+            }
+        }
+    }
+}
